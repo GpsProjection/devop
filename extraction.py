f321f28af559d5efa8fac5f1da36eb96ccfdc2eb
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2015-05-01 23:02:25 +0530

    extraction

--- extraction.py
+++ extraction.py
@@ -1,7 +1,17 @@
-import ftplib
-ftp = ftplib.FTP()
-ftp.connect('192.168.1.4', 2221)
-ftp.login('francis','francis')
-#c=ftp.dir()
-#print ftp.retrlines('LIST')
-ftp.retrbinary('RETR cache.cell', open('cache.cell', 'wb').write)
+import ftplib,cgi
+def ftpConnect(username,password,ip,port):
+	ftp = ftplib.FTP()
+	ftp.connect(str(ip),int(port))
+	ftp.login(str(username),str(password))
+	#c=ftp.dir()
+	#print ftp.retrlines('LIST')
+	ftp.retrbinary('RETR cache.cell', open('cache.cell', 'wb').write)
+
+if __name__=='__main__':
+	form = cgi.FieldStorage() 
+	username = form.getvalue('username')
+	password = form.getvalue('password')
+	ip = form.getvalue('ip')
+	port = form.getvalue('port')
+	ftpConnect(username,password,ip,port)
+
